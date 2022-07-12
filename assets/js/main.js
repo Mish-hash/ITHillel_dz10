4b0fab3c7e9b6cf751fe1e978e4ec8722bda5d38
mish-hash <poluektov23@gmail.com>
2022-07-12 20:04:51 +0300

    1. Create obj coffeMachine and update this in method start

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,39 @@
+/*
+1) Создайте объект coffeeMachine со свойством message: ‘Your coffee is ready!’ и методом start(),
+при вызове которого через 3 секунды в консоль выведется сообщение, записанное в свойстве message.
+Начальный код:
 
+let coffeeMachine = {
+   message: 'Your coffee is ready!',
+   start: function() {
+     // write your code here
+   },
+}
 
+Результат:
+coffeeMachine.start(); // 'Your coffee is ready!'
+
+Создайте объект teaPlease со свойством message: 'Wanna some tea instead of coffee?'. Обновите методу start() контекст так,
+чтобы он выводил сообщение с нового объекта.
+
+coffeeMachine.start(); // 'Wanna some tea instead of coffee?'
+
+*/
+
+const coffeeMachine = {
+    message: 'Your coffee is ready!',
+    start: function() {
+        setTimeout(()=>{
+            console.log(this.message);
+        }, 3000);
+    },
+};
+
+const teaPlease = {
+    message: 'Wanna some tea instead of coffee?',
+};
+
+coffeeMachine.start = coffeeMachine.start.bind(teaPlease);
+
+coffeeMachine.start();
 
